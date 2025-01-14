9bd4ed616bcfc42ffea691fb33fee3b34b3c46b7
BugHunter-X <askzenai@gmail.com>
2025-01-14 09:56:07 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,10 @@
+```solidity
+function transfer(address to, uint amount) public {
+  require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+  balanceOf[msg.sender] -= amount;
+  balanceOf[to] += amount;
+  emit Transfer(msg.sender, to, amount);
+}
+
+//This function does not handle the case where the 'to' address is the zero address, which is not valid.
+```
